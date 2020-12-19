14d3136cb6dbde2903c5d993da624caecdeff347
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2020-12-19 22:26:17 +0530

    updating logIn component and adding login.styles.jsx file

--- src/components/Hardik/LogIn/LogIn.jsx
+++ src/components/Hardik/LogIn/LogIn.jsx
@@ -8,40 +8,12 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Close from "@material-ui/icons/Close";
 
+import { styles } from "./LogIn.styles";
+
 import { auth } from "../../../firebase/firebase.utils";
 import { useAuthState } from "react-firebase-hooks/auth";
 
-const useStyles = makeStyles((theme) => ({
-  popUp: {
-    backgroundColor: "rgba(0,0,0,0.5)",
-    overflow: "visible",
-    position: "fixed",
-    left: "0",
-    top: "0",
-    zIndex: "100",
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  main: {
-    backgroundColor: "#212529",
-    borderRadius: "10px",
-    padding: "20px",
-  },
-  header: {
-    color: "#fff",
-    display: "flex",
-    justifyContent: "space-between",
-    marginBottom: "20px",
-  },
-  title: {
-    padding: "0",
-    margin: "0",
-    fontSize: "20px",
-  },
-}));
+const useStyles = makeStyles((theme) => styles);
 
 const LogIn = () => {
   const classes = useStyles();
@@ -73,6 +45,7 @@ const LogIn = () => {
                 style={{ cursor: "pointer" }}
               />
             </div>
+            {user ? setVisibility(false) : null}
             <SignInGoogle />
             <SignInFb />
           </div>
---
+++ src/components/Hardik/LogIn/LogIn.styles.jsx
@@ -0,0 +1,30 @@
+export const styles = {
+  popUp: {
+    backgroundColor: "rgba(0,0,0,0.5)",
+    position: "fixed",
+    left: "0",
+    top: "0",
+    zIndex: "100",
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  main: {
+    backgroundColor: "#212529",
+    borderRadius: "10px",
+    padding: "20px",
+  },
+  header: {
+    color: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    marginBottom: "20px",
+  },
+  title: {
+    padding: "0",
+    margin: "0",
+    fontSize: "20px",
+  },
+};
