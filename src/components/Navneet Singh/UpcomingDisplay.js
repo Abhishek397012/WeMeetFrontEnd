5e52141b1fd3f6028a538209f80dc1287b5acbfe
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-12-21 11:11:42 +0530

    Few updates on upcoming events section

--- src/components/Navneet Singh/UpcomingDisplay.js
+++ src/components/Navneet Singh/UpcomingDisplay.js
@@ -8,7 +8,22 @@ const UpcomingDisplay = ({setDefault}) => {
 
     return (    
         <div className="upcoming_wrapper">
-            {setDefault && showDefault()}
+            <div className="row" style={{width: "100%", margin: "0.5em"}}>
+                <div className="col-3 upcoming_left"></div>
+                <div className="col-9 upcoming_right">
+                    <div className="upcoming_right_up">
+                    </div>
+                    <div className="upcoming_right_down">
+                        <div className="urdn"></div>
+                        <div className="urdh">
+                            <div className="urdh_name">
+                            </div>
+                            <div className="urdh_button">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
