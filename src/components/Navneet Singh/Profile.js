c8697c343a20a14a520fd203c09955da3c15a9e6
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-12-22 12:51:10 +0530

    Completed initial setup of displaying the profile page of the user

--- src/components/Navneet Singh/Profile.js
+++ src/components/Navneet Singh/Profile.js
@@ -1,10 +1,53 @@
 import SidebarLayout from './Sidebar';
+import Circle from './Circle';
+import ProfileUpcoming from './ProfileUpcoming'
+import ProfilePast from './ProfilePast'
 
-const Profile = () => {
+const Profile = ({user}) => {
 
     return (    
         <SidebarLayout>
-            Profile display
+            <div className="profile_wrapper">
+                <div className="user_details">
+                    <div className="profile_header">
+                        Details
+                    </div>
+                    <div className="detail_content_profile">
+                        <Circle letter="N"/>
+                        <div className="profile_rem_content">
+                            <div className="profile_name">
+                                User Name
+                            </div>
+                            <div className="profile_other">
+                                <div className="other_stuff">
+                                    <span className="label">Designation: </span>
+                                    <span className="other_content">Student</span>
+                                </div>
+                                <div className="other_stuff">
+                                    <span className="label">Location: </span>
+                                    <span className="other_content">Delhi, India </span>
+                                </div>
+                                <div className="other_stuff">
+                                    <span className="label">Organization: </span>
+                                    <span className="other_content">NIT, Delhi </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="profile_upcoming">
+                    <div className="profile_header">
+                        Upcoming Events
+                    </div>
+                    <ProfileUpcoming />
+                </div>
+                <div className="profile_past">
+                    <div className="profile_header">
+                        Past Event
+                    </div>
+                    <ProfilePast />
+                </div>
+            </div>
         </SidebarLayout>
     )
 }
