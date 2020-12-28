420359f25141849ca76928e0c37487e2753b9f5e
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2020-12-28 22:47:27 +0530

    fixing styles

--- src/components/Navneet Singh/Sidebar.js
+++ src/components/Navneet Singh/Sidebar.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Dashboard from "./CommunityDashboard";
-import WeMeets from "./WeMeets";
 
 const Sidebar = (props) => {
   return (
